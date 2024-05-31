78efa2f569971d2e7b5fcfa750ee240a69cfc50b
Marek Bladowski <bitemnet@gmail.com>
2024-05-31 19:09:14 +0200

    Add some changes in main.py and settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+import pygame
+import os
+
 TILESIZE = 25
 ROWS = 15
 COLS = 15
@@ -6,3 +9,10 @@ WIDTH = TILESIZE * ROWS
 HEIGHT = TILESIZE * COLS
 FPS = 60
 TITLE = "Minesweeper"
+
+tile_unknown = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileUnknown.png")), (TILESIZE, TILESIZE))
+
+tile_numbers = []
+for i in range(9):
+    tile_numbers.append(
+        pygame.transform.scale(pygame.image.load(os.path.join("assets", f"Tile{i}.png")), (TILESIZE, TILESIZE)))

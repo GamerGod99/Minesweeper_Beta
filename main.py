e2d6c385412443cb8aa1a82da47ef981c7af1525
Marek Bladowski <bitemnet@gmail.com>
2024-06-06 14:37:36 +0200

    Add fun draw() & mouse event & update settings.py

--- main.py
+++ main.py
@@ -3,23 +3,35 @@ from Minesweeper import Minesweeper, Tile
 from settings import *
 
 
-pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption(TITLE)
+clock = pygame.time.Clock()
 
 ms = Minesweeper(COLS, ROWS, AMOUNT_MINES)
 
 board_surface = pygame.Surface((WIDTH, HEIGHT))
-for col in range(COLS):
-    for row in range(ROWS):
-        tile = ms.plane[col][row]
-        if tile.state == Tile.COVERED:
-            board_surface.blit(tile_unknown, (col * TILESIZE, row * TILESIZE))
-        elif tile.state == Tile.CLICKED:
-            board_surface.blit(tile_numbers[tile.value], (col * TILESIZE, row * TILESIZE))
 
-screen.blit(board_surface, (0, 0))
-pygame.display.update()
+
+def draw():
+    for y in range(COLS):
+        for x in range(ROWS):
+            tile = ms.plane[y][x]
+            if tile.state == Tile.COVERED:
+                board_surface.blit(tile_unknown, (x * TILESIZE, y * TILESIZE))
+            elif tile.state == Tile.CLICKED:
+                board_surface.blit(tile_numbers[tile.value], (x * TILESIZE, y * TILESIZE))
+            elif tile.state == Tile.EXPLOSION:
+                board_surface.blit(tile_exploded, (x * TILESIZE, y * TILESIZE))
+            elif tile.state == Tile.NOT_MINE:
+                board_surface.blit(tile_not_mine, (x * TILESIZE, y * TILESIZE))
+            elif tile.state == Tile.FLAGGED:
+                board_surface.blit(tile_flag, (x * TILESIZE, y * TILESIZE))
+    screen.blit(board_surface, (0, 0))
+
+
+draw()
+
+pygame.display.flip()
 
 while True:
     for event in pygame.event.get():
@@ -27,4 +39,8 @@ while True:
             pygame.quit()
             quit(0)
 
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mx, my = pygame.mouse.get_pos()
+            ms.click(my // TILESIZE, mx // TILESIZE, event.button)
+            draw()
+            pygame.display.flip()

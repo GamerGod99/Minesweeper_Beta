7c663e88a0b34aaf453c8e8286f5ee4bd4e4e8c5
Marek Bladowski <bitemnet@gmail.com>
2024-06-21 15:29:21 +0200

    Almost final version

--- main.py
+++ main.py
@@ -6,17 +6,26 @@ from settings import *
 class Game:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pygame.display.set_mode((W, H1 + H2))
         pygame.display.set_caption(TITLE)
         self.clock = pygame.time.Clock()
 
     def board(self):
         self.ms = Minesweeper(COLS, ROWS, AMOUNT_MINES)
-        self.board_surface = pygame.Surface((WIDTH, HEIGHT))
+        self.score_surface = pygame.Surface((W, H1))
+        self.board_surface = pygame.Surface((W, H2))
         self.draw()
         pygame.display.flip()
 
     def draw(self):
+        self.score_surface.fill(BACKGROUND_COLOR)
+        self.score_surface.blit(button_off, (X_RESET_BUTTON, Y_RESET_BUTTON))
+        self.nr_flags = pygame.font.Font.render(pygame.font.SysFont("mono", FONT_SIZE), f"{self.ms.flags:>03}", True, (255, 0, 0))
+        self.time_text = pygame.font.Font.render(pygame.font.SysFont("mono", FONT_SIZE), "000", True, (255, 0, 0))
+        self.score_surface.blit(self.nr_flags, (20, (H1 - FONT_SIZE) // 2))
+        self.score_surface.blit(self.time_text, (W - self.time_text.get_width() - 20, (H1 - FONT_SIZE) // 2))
+        self.screen.blit(self.score_surface, (0, 0))
+
         for y in range(COLS):
             for x in range(ROWS):
                 tile = self.ms.plane[y][x]
@@ -30,26 +39,31 @@ class Game:
                     self.board_surface.blit(tile_not_mine, (x * TILESIZE, y * TILESIZE))
                 elif tile.state == Tile.FLAGGED:
                     self.board_surface.blit(tile_flag, (x * TILESIZE, y * TILESIZE))
-        self.screen.blit(self.board_surface, (0, 0))
+        self.screen.blit(self.board_surface, (0, H1))
 
     def run(self):
         mouse_on = True
-        while True:
+        game_on = True
+        while game_on:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit(0)
 
-                if mouse_on and event.type == pygame.MOUSEBUTTONDOWN:
+                if event.type == pygame.MOUSEBUTTONDOWN:
                     mx, my = pygame.mouse.get_pos()
-
-                    if self.ms.click(my // TILESIZE, mx // TILESIZE, event.button):
-                        mouse_on = False
+                    if (X_RESET_BUTTON <= mx < (X_RESET_BUTTON + W_RESET_BUTTON)) and (Y_RESET_BUTTON <= my < (Y_RESET_BUTTON + H_RESET_BUTTON)):
+                        game_on = False
+                        break
+                    elif mouse_on and H1 <= my < H1 + H2:
+                        if self.ms.click((my - H1) // TILESIZE, mx // TILESIZE, event.button):
+                            mouse_on = False
                     self.draw()
                     pygame.display.flip()
 
 
 if __name__ == "__main__":
-    game = Game()
-    game.board()
-    game.run()
+    while True:
+        game = Game()
+        game.board()
+        game.run()
--- settings.py
+++ settings.py
@@ -4,11 +4,14 @@ import os
 TILESIZE = 32
 ROWS = 15
 COLS = 15
-AMOUNT_MINES = 5
-WIDTH = TILESIZE * ROWS
-HEIGHT = TILESIZE * COLS
+AMOUNT_MINES = 40
+W = TILESIZE * ROWS
+H1 = TILESIZE * 3
+H2 = TILESIZE * COLS
 FPS = 60
 TITLE = "Minesweeper Clone"
+BACKGROUND_COLOR = (192, 192, 192)
+FONT_SIZE = 60
 
 tile_numbers = []
 for i in range(10):
@@ -18,3 +21,10 @@ tile_exploded = pygame.transform.scale(pygame.image.load(os.path.join("assets", 
 tile_flag = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileFlag.png")), (TILESIZE, TILESIZE))
 tile_unknown = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileUnknown.png")), (TILESIZE, TILESIZE))
 tile_not_mine = pygame.transform.scale(pygame.image.load(os.path.join("assets", "TileNotMine.png")), (TILESIZE, TILESIZE))
+
+X_RESET_BUTTON, Y_RESET_BUTTON = (W - TILESIZE * 2) // 2, (H1 - TILESIZE * 2) // 2
+W_RESET_BUTTON, H_RESET_BUTTON = TILESIZE * 2, TILESIZE * 2
+button_off = pygame.transform.scale(pygame.image.load(os.path.join("assets", "button_off.png")), (W_RESET_BUTTON, H_RESET_BUTTON))
+button_on = pygame.transform.scale(pygame.image.load(os.path.join("assets", "button_on.png")), (W_RESET_BUTTON, H_RESET_BUTTON))
+button_o = pygame.transform.scale(pygame.image.load(os.path.join("assets", "button_o.png")), (W_RESET_BUTTON, H_RESET_BUTTON))
+button_x = pygame.transform.scale(pygame.image.load(os.path.join("assets", "button_x.png")), (W_RESET_BUTTON, H_RESET_BUTTON))
